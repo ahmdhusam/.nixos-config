3bcf69cea39842e5ca043de5d3399b650a10b6ce
Ahmed <ahmdhusam7@gmail.com>
2024-11-09 05:27:20 +0200

    Update: :zap: root.nix

--- modules/nixos/root.nix
+++ modules/nixos/root.nix
@@ -23,6 +23,7 @@ in
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users = {
       defaultUserShell = pkgs.zsh;
+      mutableUsers = false;
       users = {
 
         ${cfg.userName} = {
@@ -32,6 +33,7 @@ in
           description = "${cfg.userName}";
           home = "/home/${cfg.userName}";
           hashedPassword = "$6$SxtdwjPfPb8e6xqt$llLQP3l9IfguG76I8s6h1KpMI9hBiGK.DIAKADK0S/Ein9tZqdWt.O96BA0KlijUOwgzCwTAQC1tEgBr0xWLW1";
+          initialHashedPassword = "$6$SxtdwjPfPb8e6xqt$llLQP3l9IfguG76I8s6h1KpMI9hBiGK.DIAKADK0S/Ein9tZqdWt.O96BA0KlijUOwgzCwTAQC1tEgBr0xWLW1";
           extraGroups = [
             "networkmanager"
             "wheel"
